f8767ebf13caa65ef8070e332e80bcc8d706ffe2
Pranjal Jumde <pranjal.jumde@gmail.com>
2020-01-29 13:12:21 -0800

    Fix 7522: Disable WebSQL

--- app/brave_main_delegate.cc
+++ app/brave_main_delegate.cc
@@ -150,6 +150,8 @@ bool BraveMainDelegate::BasicStartupComplete(int* exit_code) {
   command_line.AppendSwitchASCII(switches::kSyncServiceURL,
                                  "https://no-thanks.invalid");
 
+  command_line.AppendSwitch(kDisableDatabases);
+
   // Enabled features.
   const std::unordered_set<const char*> enabled_features = {
       password_manager::features::kPasswordImport.name,
